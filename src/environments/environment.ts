d31faba77f08abad9e25d9c25f4381203f7022b1
myportafoliobryantnzapp <bry.tandazo@gmail.com>
2022-12-09 09:53:43 -0500

    Clase6_1Parte

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapsKey: 'AIzaSyAW1vbFj801JyfqCxt_MGdEiTWeCqy5IRs' // Mod 3
 };
 
 /*
